d113f3622293f8e90f7adecbc079ae4238f8e80a
Douglas Ludlow <doug.ludlow@royaljay.net>
2016-05-17 16:28:44 -0600

    Add bootswatch theme.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,26 @@
 'use strict';
- 
+
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
- 
+    sass = require('gulp-sass'),
+    cleanCss = require('gulp-clean-css');
+
 var config = {
     styles: './client/styles/',
     sass: './client/styles/*.scss'
 };
- 
+
 gulp.task('sass', function () {
-  return gulp.src(config.sass)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(config.styles));
+    return gulp.src(config.sass)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCss({ debug: true }, function (details) {
+            console.log(details.name + ': ' + details.stats.originalSize);
+            console.log(details.name + ': ' + details.stats.minifiedSize);
+        }))
+        .pipe(gulp.dest(config.styles));
 });
- 
+
 gulp.task('sass:watch', function () {
-  gulp.watch(config.sass, ['sass']);
+    gulp.watch(config.sass, ['sass']);
 });
 
 gulp.task('default', ['sass']);
