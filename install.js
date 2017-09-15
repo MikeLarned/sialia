87bdcb6ad5a9f9a8e41b710fae2167f967e4986a
Lucas Obendorf <lucas.obendorf@gmail.com>
2017-09-15 16:00:03 -0600

    The install script now sets its exit code equal to the exit code of its pack command result.

--- install.js
+++ install.js
@@ -1,8 +1,8 @@
 const exec = require('child_process').exec;
 
 exec('npm run pack', (err, stdout, stderr) => {
-    if (stderr) console.log(stderr);
+    if (stderr) console.error(stderr);
     if (err) console.error(err);
 }).on('exit', code => {
-    if(code) console.log("Installer exited with code: " + code);
+    process.exitCode = code;
 });
