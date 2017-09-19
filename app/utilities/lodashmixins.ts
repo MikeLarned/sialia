41da52d95ffe60c9d2a6fb754acb9822d837c2b6
Lucas Obendorf <lucas.obendorf@gmail.com>
2017-09-19 10:27:40 -0600

    Renamed lodash to see if it exports properly

--- app/utilities/lodashmixins.ts
+++ app/utilities/lodashmixins.ts
@@ -1,8 +1,8 @@
-import _ from 'lodash';
+import * as lodashFull from 'lodash';
 
-module.exports = _;
+module.exports = lodashFull;
 
-_.mixin({
+lodashFull.mixin({
     move: function (array, fromIndex, toIndex) {
 	    array.splice(toIndex, 0, array.splice(fromIndex, 1)[0] );
 	    return array;
