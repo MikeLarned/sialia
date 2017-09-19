9fbc6fda454a2e892bcfcbb3bfc3814077ff581e
Lucas Obendorf <lucas.obendorf@gmail.com>
2017-09-19 09:58:30 -0600

    Changed lodash version removed the explicit library name in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "index.js",
-        library: "ccdaview",
+        //library: "ccdaview",
         libraryTarget: "umd"
     },
     resolve: {
