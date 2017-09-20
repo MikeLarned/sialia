d92dd089250a7fd03b49c1003e952cbdd279de0f
Lucas Obendorf <lucas.obendorf@gmail.com>
2017-09-20 14:09:22 -0600

    Using BlueButton.parseString instead of BlueButton.parse

--- app/utilities/lodashmixins.ts
+++ app/utilities/lodashmixins.ts
@@ -1,6 +1,6 @@
-import * as lodash from 'lodash';
+import _ from 'lodash';
 
-lodash.mixin({
+_.mixin({
     move: function (array, fromIndex, toIndex) {
 	    array.splice(toIndex, 0, array.splice(fromIndex, 1)[0] );
 	    return array;
