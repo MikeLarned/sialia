9392bcbaef114f5bbb98cf6c9795e7822a48c9ce
Lucas Obendorf <lucas.obendorf@gmail.com>
2017-09-25 10:34:11 -0600

    Added an optional error handling callback option to the app

--- app/app.ts
+++ app/app.ts
@@ -10,21 +10,25 @@ export class App {
 
   service: DocumentsService = new DocumentsService();
 
-  constructor(options: any) {
+  constructor(options: any, errorHandler?: (err: string) => void) {
 
     let documents = options.docs;
 
     if (lodash.isString(documents[0])) {
-      this.service.loadRaw(documents[0]).subscribe(documents => {
-        riot.mount('sialia', {
-          documents: documents
-        });
+      this.service.loadRaw(documents[0])
+        .subscribe(documents => {
+          riot.mount('sialia', {
+            documents: documents
+          },
+          errorHandler);
       });
     } else {
-      this.service.fetch(documents[0].Url).subscribe((options) => {
-        options.documents = documents;
-        riot.mount('sialia', options);
-      });
+      this.service.fetch(documents[0].Url)
+        .subscribe((options) => {
+          options.documents = documents;
+          riot.mount('sialia', options);
+        },
+        errorHandler);
     }
   }
 }
