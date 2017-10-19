fdeb0c566d34735dd2c4bea863f54b13f2a0e75e
Lucas Obendorf <lucas.obendorf@gmail.com>
2017-10-19 15:59:03 -0600

    Package updates. Removed bluebird whitelist

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack                 = require('webpack'),
     FixDefaultImportPlugin  = require('webpack-fix-default-import-plugin');
 
 let excludes = nodeExternals({
-    whitelist: ["bluebutton"]
+    //whitelist: ["bluebutton"]
 });
 
 /*excludes.lodash = {
