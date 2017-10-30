04ff6e886bd2363147ec28083cf78a75260737fe
Lucas Obendorf <lucas.obendorf@gmail.com>
2017-10-30 17:11:36 -0600

    Remove mount call in ccda-section for more debugging.

--- dist/sialia.js
+++ dist/sialia.js
@@ -6037,8 +6037,8 @@ riot.tag2('panel', '<div class="panel panel-{opts.state ? opts.state : \'default
     data: opts.parent.data[opts.current.key]
   };
 
-  console.log("Mounting");
-  riot.mount(this.root, opts.current.tagName, options);
+  //console.log("Mounting");
+  //riot.mount(this.root, opts.current.tagName, options);
   this.on('update', function() {
     options.data = opts.parent.data[opts.current.key];
   });
